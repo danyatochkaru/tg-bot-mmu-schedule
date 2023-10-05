ab1b7eca52484deecaa129705e356de9b5cee30d
Даня <45875910+danyatochkaru@users.noreply.github.com>
2023-10-05 12:16:23 +0300

    fix: добавил обработку ошибок, убивающих процесс

--- src/menu/menu.update.ts
+++ src/menu/menu.update.ts
@@ -3,9 +3,12 @@ import { Context } from 'telegraf';
 import { theMenu } from './menu.buttons';
 import { editMessage } from '../utils/editMessage';
 import { MESSAGES } from '../app.constants';
+import { Logger } from '@nestjs/common';
 
 @Update()
 export class MenuUpdate {
+  private logger = new Logger(MenuUpdate.name);
+
   @Command(/menu/i)
   @Action('menu')
   async sendMenu(@Ctx() ctx: Context) {
@@ -14,9 +17,11 @@ export class MenuUpdate {
         reply_markup: theMenu().reply_markup,
       });
     } else {
-      await ctx.reply(MESSAGES['ru'].MENU, {
-        reply_markup: theMenu().reply_markup,
-      });
+      await ctx
+        .reply(MESSAGES['ru'].MENU, {
+          reply_markup: theMenu().reply_markup,
+        })
+        .catch((err) => this.logger.error(err));
     }
   }
 }
--- src/utils/editMessage.ts
+++ src/utils/editMessage.ts
@@ -7,24 +7,28 @@ export function editMessage(
   options?: ExtraEditMessageText,
   message?: { message_id: number },
 ) {
-  if (ctx && !ctx.callbackQuery) {
-    if (!message) {
-      return false;
+  try {
+    if (ctx && !ctx.callbackQuery) {
+      if (!message) {
+        return false;
+      }
+      return ctx.telegram.editMessageText(
+        ctx.chat.id,
+        message.message_id,
+        undefined,
+        text,
+        options,
+      );
     }
+
     return ctx.telegram.editMessageText(
       ctx.chat.id,
-      message.message_id,
+      ctx.callbackQuery.message.message_id,
       undefined,
       text,
       options,
     );
+  } catch (err: any) {
+    console.log(`Failed to edit message for ${ctx.chat.id}`, err);
   }
-
-  return ctx.telegram.editMessageText(
-    ctx.chat.id,
-    ctx.callbackQuery.message.message_id,
-    undefined,
-    text,
-    options,
-  );
 }
