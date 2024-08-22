cc8a241de139230a8c53768f41f24cf6e6f5447f
Daniil <support@danyatochka.ru>
2024-08-22 17:43:45 +0300

    fix: убрал локальный путь из lint конфига

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,38 +9,45 @@ import { FlatCompat } from '@eslint/eslintrc';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["**/.eslintrc.js"],
-}, ...compat.extends("plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"), {
+export default [
+  {
+    ignores: ['**/.eslintrc.js'],
+  },
+  ...compat.extends(
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ),
+  {
     plugins: {
-        "@typescript-eslint": eslintPlugin,
+      '@typescript-eslint': eslintPlugin,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.jest,
-        },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
 
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'module',
 
-        parserOptions: {
-            project: "tsconfig.json",
-            tsconfigRootDir: "F:\\projects\\mmu\\bot",
-        },
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
     },
 
     rules: {
-        "@typescript-eslint/interface-name-prefix": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-explicit-any": "off",
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
-}];
+  },
+];
