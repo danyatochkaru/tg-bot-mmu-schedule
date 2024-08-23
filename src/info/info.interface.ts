7cb63db3bb25dc1119b26142cd05869327813a5d
Daniil <support@danyatochka.ru>
2024-08-23 14:00:34 +0300

    feat: обновил алгоритм сбора данных

--- src/info/info.interface.ts
+++ src/info/info.interface.ts
@@ -1,14 +1,23 @@
+import { UserEntity } from '../users/user.entity';
+
 export class InfoUsersObject {
   date?: Date;
   dates?: [Date, Date];
   total_count: number;
   details?: Array<{
-    groups?: Array<{
-      group_id?: number;
-      group_name?: string;
-      count?: number;
-    }>;
-    count: number;
+    groups: Array<
+      Pick<
+        UserEntity,
+        | 'group_id'
+        | 'group_name'
+        | 'is_inactive'
+        | 'inactive_reason'
+        | 'register_source'
+        | 'created_at'
+      > & {
+        count: number;
+      }
+    >;
     date: Date;
     scale: 'day' | 'week' | 'month';
   }>;
--- src/info/info.service.ts
+++ src/info/info.service.ts
@@ -28,47 +28,59 @@ export class InfoService {
         month: [startOfMonth, endOfMonth],
       };
 
-      const [groups, count] =
-        await this.usersService.getGroupsWithCountNewUsers([
-          dateFns[scale][0](date),
-          dateFns[scale][1](date),
-        ]);
+      const [users, count] = await this.usersService.getGroupsWithCountNewUsers(
+        [dateFns[scale][0](date), dateFns[scale][1](date)],
+      );
 
       result.total_count = count;
-      result.details = groups.reduce(
-        (acc, group) => {
-          if (
-            acc.some((i) => startOfDay(i.date) === startOfDay(group.created_at))
-          ) {
-            const index = acc.findIndex(
-              (i) => startOfDay(i.date) === startOfDay(group.created_at),
-            );
-            acc[index].count += 1;
-            if (acc[index].groups.some((i) => i.group_id === group.group_id)) {
+      result.details = users
+        .map((user) => startOfDay(user.created_at))
+        .filter((x, i, a) => a.indexOf(x) === i)
+        .map((date) => ({
+          groups: users
+            .filter((u) => startOfDay(u.created_at) === date)
+            .map((u, _, a) =>
+              Object.assign(u, {
+                count: a.reduce(
+                  (acc, cur) => (cur.group_id === u.group_id ? acc + 1 : acc),
+                  1,
+                ),
+              }),
+            ),
+          date,
+          scale,
+        }));
+      /*result.details = users.reduce(
+        (acc, user) => {
+          const findEqDatesFn = (i) =>
+            startOfDay(i.date) === startOfDay(user.created_at);
+
+          if (acc.some(findEqDatesFn)) {
+            const index = acc.findIndex(findEqDatesFn);
+
+            const findEqGroupsFn = (i) => i.group_id === user.group_id;
+            if (acc[index].groups.some(findEqGroupsFn)) {
               acc[index].groups[
-                acc[index].groups.findIndex(
-                  (i) => i.group_id === group.group_id,
-                )
+                acc[index].groups.findIndex(findEqGroupsFn)
               ].count += 1;
             }
           } else {
             acc.push({
               groups: [
                 {
-                  group_id: group.group_id,
-                  group_name: group.group_name,
+                  group_id: user.group_id,
+                  group_name: user.group_name,
                   count: 1,
                 },
               ],
-              count: 1,
-              date: startOfDay(group.created_at),
+              date: startOfDay(user.created_at),
               scale,
             });
           }
           return acc;
         },
         [] as InfoUsersObject['details'],
-      );
+      );*/
     } else {
       result.total_count = await this.usersService.getCount();
     }
