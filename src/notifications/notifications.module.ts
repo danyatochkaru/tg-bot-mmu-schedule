98b5c9c36f39e8894ab787064201f7759703a95c
Daniil <support@danyatochka.ru>
2024-11-18 18:21:23 +0300

    feat(notifications): первая инъекция вызова веб хуков и ручное прерывание рассылки

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 import { UsersModule } from '../users/users.module';
+import { WebhookModule } from '../webhook/webhook.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, WebhookModule, HttpModule],
   controllers: [NotificationsController],
   providers: [NotificationsService],
   exports: [NotificationsService],
